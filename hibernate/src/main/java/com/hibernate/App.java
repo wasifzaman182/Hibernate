23d35823830830e27f84bd61fb1a050f74d4b8f9
Wasif Zaman <75499379+wasifzaman182@users.noreply.github.com>
2021-10-02 04:29:00 -0700

    Insert first object using hibernate

--- hibernate/src/main/java/com/hibernate/App.java
+++ hibernate/src/main/java/com/hibernate/App.java
@@ -1,7 +1,13 @@
 package com.hibernate;
 
+
+
+import org.hibernate.Session;
 import org.hibernate.SessionFactory;
+import org.hibernate.Transaction;
 import org.hibernate.cfg.Configuration;
+
+import model.Student;
 
 /**
  * Hello world!
@@ -9,12 +15,23 @@ import org.hibernate.cfg.Configuration;
  */
 public class App 
 {
-    public static void main( String[] args )
+    public static void main( String[] args ) throws Exception
     {
         System.out.println( "Hello World!" );
         
         SessionFactory factory = new Configuration().configure().buildSessionFactory();
         
-        System.out.println("Session factory ::" + factory);
+        Student s = new Student();
+        s.setId(2);
+        s.setCity("peshawar");
+        s.setName("wasif");
+        
+        Session session = factory.openSession();
+        
+        Transaction tran = session.beginTransaction();
+        session.save(s);
+        tran.commit();
+        session.close();
+        //System.out.println("Session factory ::" + factory);
     }
 }
