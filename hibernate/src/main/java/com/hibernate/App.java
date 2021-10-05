9705baf01346ae11f8aa7e125be1a5815f99faac
Wasif Zaman <75499379+wasifzaman182@users.noreply.github.com>
2021-10-05 09:57:59 -0700

    New Class added and some annotations

--- hibernate/src/main/java/com/hibernate/App.java
+++ hibernate/src/main/java/com/hibernate/App.java
@@ -2,11 +2,16 @@ package com.hibernate;
 
 
 
+import java.io.FileInputStream;
+import java.io.FileReader;
+import java.util.Date;
+
 import org.hibernate.Session;
 import org.hibernate.SessionFactory;
 import org.hibernate.Transaction;
 import org.hibernate.cfg.Configuration;
 
+import model.Address;
 import model.Student;
 
 /**
@@ -22,14 +27,26 @@ public class App
         SessionFactory factory = new Configuration().configure().buildSessionFactory();
         
         Student s = new Student();
-        s.setId(2);
         s.setCity("peshawar");
         s.setName("wasif");
         
-        Session session = factory.openSession();
+        Address ad = new Address();
+        ad.setCity("KARACHI");
+        ad.setStreet("Street1");
+        ad.setOpen(true);
+        ad.setAddedDate(new Date());
+        ad.setX(79.475983);
         
+        //reading file
+        FileInputStream file = new FileInputStream("src/main/java/3rd class summary.PNG");
+        byte[] data = new  byte[file.available()];
+        file.read();
+        ad.setImage(data);
+        
+        Session session = factory.openSession();
         Transaction tran = session.beginTransaction();
         session.save(s);
+        session.save(ad);
         tran.commit();
         session.close();
         //System.out.println("Session factory ::" + factory);
